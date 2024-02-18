4d2f115125c01c34a67311181767a60b0161447d
pablo perez <pperez@nextlane.com>
2024-02-18 22:32:10 +0100

    adding theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 void main() {
   runApp(const WidgetsApp());
@@ -11,6 +12,7 @@ class WidgetsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 10).getTheme(),
       home: Scaffold(
           appBar: AppBar(
             title: const Text('WidgetsApp'),
