c44854187428daa72c53cd2d04321c0e9f8ff4ba
pablo perez <pperez@nextlane.com>
2024-02-18 22:54:41 +0100

    home screen and listView

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,9 +12,52 @@ class HomeScreen extends StatelessWidget {
           'Flutter + Material 3',
         ),
       ),
-      body: const Center(
-        child: Text('Hello, World!'),
+      body: const _HomeLayput(),
+    );
+  }
+}
+
+class _HomeLayput extends StatelessWidget {
+  const _HomeLayput();
+
+  @override
+  Widget build(BuildContext context) {
+    const menuItems = appMenuItems;
+    return ListView.builder(
+        physics: const ClampingScrollPhysics(),
+        itemBuilder: (context, index) {
+          final item = menuItems[index];
+          return _HomeItemView(item: item);
+        },
+        itemCount: menuItems.length);
+  }
+}
+
+class _HomeItemView extends StatelessWidget {
+  const _HomeItemView({
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(item.title),
+      subtitle: Text(item.subttitle),
+      trailing: Icon(
+        Icons.chevron_right,
+        color: colors.primary,
       ),
+      leading: Icon(
+        item.icon,
+        color: colors.primary,
+      ),
+      onTap: () {
+        //TODO: Navigate to the selected item
+      },
     );
   }
 }
