dc3a9a4beab8289453a1fbde94236aa4e2c7ad2d
pablo perez <pperez@nextlane.com>
2024-02-18 22:21:51 +0100

    removing debug bannner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class WidgetsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: const Text('WidgetsApp'),
